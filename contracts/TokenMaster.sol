dabb8554b09948d70f0885815914714201c8d6f0
Will Bryan Jose <willjose51@gmail.com>
2024-03-14 20:35:03 +0800

    Finish events functions

--- contracts/TokenMaster.sol
+++ contracts/TokenMaster.sol
@@ -4,10 +4,55 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TokenMaster is ERC721 {
+    address public owner;
+    uint public totalOccasions;
+
+    struct Occasion {
+        uint256 id;
+        string name;
+        uint cost;
+        uint tickets;
+        uint maxTickets;
+        string date;
+        string time;
+        string location;
+    }
+
+    mapping(uint256 => Occasion) occasions;
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
     constructor(
         string memory _name,
         string memory _symbol
     ) ERC721(_name, _symbol) {
-        
+        owner = msg.sender;
+    }
+
+    function list(
+        string memory _name,
+        uint256 _cost,
+        uint256 _maxTickets,
+        string memory _date,
+        string memory _time,
+        string memory _location
+    ) public onlyOwner{
+        totalOccasions++;
+        occasions[totalOccasions] = Occasion(
+            totalOccasions,
+            _name,
+            _cost,
+            _maxTickets,
+            _maxTickets,
+            _date,
+            _time,
+            _location
+        );
+    }
+
+    function getOccasion(uint256 _id) public view returns (Occasion memory) {
+        return occasions[_id];
     }
 }
