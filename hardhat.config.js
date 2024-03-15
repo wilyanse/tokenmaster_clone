3aacfe95f4f11793031b5c1bcfdceec2ac2e9faa
Will Bryan Jose <willjose51@gmail.com>
2024-03-15 10:47:46 +0800

    Finish and verify smart contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,35 @@
+const { HardhatUserConfig } = require("hardhat/config");
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
+const ETHER_API_KEY = "EBYUE4MMHKBCZQU5IMSRZZ9TB7ZESCI2PB";
+const ARBITRUM_SEPOLIA_URL = "https://sepolia-rollup.arbitrum.io/rpc";
+const PRIVATE_KEY = "04f0856be7787855951c21d3f2f4d25b6eb9f20c7f243ecaf2e6cb4066e54686";
+
+const config = {
   solidity: "0.8.24",
+  networks: {
+    arbitrumSepolia: {
+      url: ARBITRUM_SEPOLIA_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 421614,
+    },
+  },
+  sourcify: {
+    enabled: true,
+  },
+  etherscan: {
+    apiKey: ETHER_API_KEY,
+    customChains: [
+      {
+        network: "arbsep",
+        chainId: 421614,
+        urls: {
+          apiURL: "https://api-sepolia.arbiscan.io/api",
+          browserURL: "https://sepolia.arbiscan.io/",
+        },
+      },
+    ],
+  }
 };
+
+module.exports = config;
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,82 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const hre = require("hardhat")
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+async function main() {
+  // Setup accounts & variables
+  const [deployer] = await ethers.getSigners()
+  const NAME = "TokenMaster"
+  const SYMBOL = "TM"
+
+  // Deploy contract
+  const TokenMaster = await ethers.getContractFactory("TokenMaster")
+  const tokenMaster = await TokenMaster.deploy(NAME, SYMBOL)
+  await tokenMaster.deployed()
+
+  console.log(`Deployed TokenMaster Contract at: ${tokenMaster.address}\n`)
+
+  // List 6 events
+  const occasions = [
+    {
+      name: "UFC Miami",
+      cost: tokens(3),
+      tickets: 0,
+      date: "May 31",
+      time: "6:00PM EST",
+      location: "Miami-Dade Arena - Miami, FL"
+    },
+    {
+      name: "ETH Tokyo",
+      cost: tokens(1),
+      tickets: 125,
+      date: "Jun 2",
+      time: "1:00PM JST",
+      location: "Tokyo, Japan"
+    },
+    {
+      name: "ETH Privacy Hackathon",
+      cost: tokens(0.25),
+      tickets: 200,
+      date: "Jun 9",
+      time: "10:00AM TRT",
+      location: "Turkey, Istanbul"
+    },
+    {
+      name: "Dallas Mavericks vs. San Antonio Spurs",
+      cost: tokens(5),
+      tickets: 0,
+      date: "Jun 11",
+      time: "2:30PM CST",
+      location: "American Airlines Center - Dallas, TX"
+    },
+    {
+      name: "ETH Global Toronto",
+      cost: tokens(1.5),
+      tickets: 125,
+      date: "Jun 23",
+      time: "11:00AM EST",
+      location: "Toronto, Canada"
+    }
+  ]
+
+  for (var i = 0; i < 5; i++) {
+    const transaction = await tokenMaster.connect(deployer).list(
+      occasions[i].name,
+      occasions[i].cost,
+      occasions[i].tickets,
+      occasions[i].date,
+      occasions[i].time,
+      occasions[i].location,
+    )
+
+    await transaction.wait()
+
+    console.log(`Listed Event ${i + 1}: ${occasions[i].name}`)
+  }
+}
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
